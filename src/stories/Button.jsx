cafdec35811813ce2d1a1f0096048d7381469889
“Betty72” <betisasg@gmail.com>
2022-01-04 11:52:29 +0100

    bugfix_3

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}this a bug
+      {label}
     </button>
   );
 };
